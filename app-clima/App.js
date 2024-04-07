791ebe1ddc24f3dec44202af095754c14edc8e6f
lorrayneferreira18 <lorraynefd15@gmail.com>
2024-04-07 00:28:17 -0300

    implementação api

--- app-clima/App.js
+++ app-clima/App.js
@@ -1,50 +1,84 @@
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, View, Text, Image } from 'react-native'
 
+import * as Location from 'expo-location'
+import getCurrentWeather from '../app-clima/api/index'
+
 const App = () => {
-  const temperaturaAtual = 10
-  const localizacao = 'Sua Localização'
-  const vento = '10 km/h'
-  const umidade = '70%'
-  const temperaturaMinima = '20°C'
-  const temperaturaMaxima = '30°C'
+  const axios = require('axios')
+  const [currentTemperature, setCurrentTemperature] = useState('31')
+
+  const [locationCoords, setLocationCoords] = useState(null)
+
+  const [locationName, setLocationName] = useState('Brasil, Ribeirão das Neves')
+
+  const [temperatureMin, setTemperatureMin] = useState('21')
+  const [temperatureMax, setTemperatureMax] = useState('32')
+  const [wind, setWind] = useState('7')
+  const [humidity, setHumidity] = useState('68')
 
   // Função para determinar o ícone com base na temperatura
   const renderizarIconeClima = () => {
-    if (temperaturaAtual > 20) {
+    if (currentTemperature > 20) {
       return <Image source={require('../app-clima/assets/sol.png')} style={styles.iconeClima} />
     } else {
       return <Image source={require('../app-clima/assets/neve.png')} style={styles.iconeClima} />
     }
   }
+  const WeatherInfoCard = ({ title, value }) => {
+    return (
+      <View style={styles.card}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.value}>{value}</Text>
+      </View>
+    )
+  }
+  async function getLocation() {
+    const { status } = await Location.requestForegroundPermissionsAsync()
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied')
+    } else {
+      const location = await Location.getCurrentPositionAsync({})
+      await setLocationCoords(location.coords)
+    }
+  }
+
+  async function setCurrentWeather() {
+    await getLocation()
+    const data = await getCurrentWeather(locationCoords)
+
+    // Vem da api nessa ordem [currentTemperature, temperatureMin, temperatureMax, locationName, wind, humidity]
+
+    setCurrentTemperature(convertKelvinToC(data[0]))
+    setTemperatureMin(convertKelvinToC(data[1]))
+    setTemperatureMax(convertKelvinToC(data[2]))
+    setLocationName(data[3])
+    setWind(data[4])
+    setHumidity(data[5])
+  }
+  function convertKelvinToC(kelvin) {
+    return parseInt(kelvin - 273)
+  }
+  useEffect(() => {
+    setCurrentWeather()
+  }, [])
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.titulo}>Previsão do Tempo</Text>
         {renderizarIconeClima()}
-        <Text style={styles.temperatura}>{temperaturaAtual}°C</Text>
-        <Text style={styles.localizacao}>{localizacao}</Text>
+        <Text style={styles.temperatura}>{currentTemperature}°C</Text>
+        <Text style={styles.localizacao}>{locationName}</Text>
       </View>
-      <View style={styles.informacoesClima}>
-        <View style={styles.card}>
-          <Text style={styles.infoTitulo}>Vento</Text>
-          <Text style={styles.infoTexto}>{vento}</Text>
-        </View>
-        <View style={styles.card}>
-          <Text style={styles.infoTitulo}>Umidade do Ar</Text>
-          <Text style={styles.infoTexto}>{umidade}</Text>
-        </View>
-        <View style={styles.card}>
-          <Text style={styles.infoTitulo}>Temp. Mínima</Text>
-          <Text style={styles.infoTexto}>{temperaturaMinima}</Text>
-        </View>
-        <View style={styles.card}>
-          <Text style={styles.infoTitulo}>Temp. Máxima</Text>
-          <Text style={styles.infoTexto}>{temperaturaMaxima}</Text>
-        </View>
+      <View style={styles.containerCars}>
+        <WeatherInfoCard title='Vento' value={wind} />
+        <WeatherInfoCard title='Umidade' value={humidity} />
+        <WeatherInfoCard title='Mínima' value={temperatureMin} />
+        <WeatherInfoCard title='Máxima' value={temperatureMax} />
       </View>
+
     </View>
   )
 }
@@ -99,6 +133,20 @@ const styles = StyleSheet.create({
   iconeClima: {
     width: 100,
     height: 100
+  },
+  containerCars: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 10
+  },
+  title: {
+    fontSize: 16,
+    marginBottom: 5,
+    color: '#6a5acd'
+  },
+  value: {
+    fontSize: 12
   }
 })
 
